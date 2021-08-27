71fa97efd71862b619c688f38bba7cc0d74bab65
unknown <huzaifahhabib9@>
2021-08-27 07:44:42 +0500

    madlibs game

---
+++ main.py
@@ -0,0 +1,8 @@
+Full_name = input("Enter your full name: ")
+city = input("Enter your city: ")
+current_status= input("Enter what you are currently: ")
+institute_name= input("enter your institute name:")
+goal = input("Enter your goal : ")
+
+paragraph = "Hello! my name is "+ Full_name + " from " + city + ". I am currently a " + current_status + " of software engineering at " + institute_name + ". I want to become a " + goal + "."
+print(paragraph)
